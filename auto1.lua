fdfe1435a3ed10360f4d35ff88bd962b5aa0e90e
Davi99999 <140989331+Davi99999@users.noreply.github.com>
2023-11-25 17:18:22 -0300

    Create auto1.lua

---
+++ auto1.lua
@@ -0,0 +1,104 @@
+while wait() do
+pcall(function()
+
+
+local args = {
+    [1] = "f2r2",
+    [2] = {
+        ["Level"] = 1
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_voting-system@0.1.11"):WaitForChild("voting-system"):WaitForChild("__comm__"):WaitForChild("RE"):WaitForChild("Vote"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "Ready"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_voting-system@0.1.11"):WaitForChild("voting-system"):WaitForChild("__comm__"):WaitForChild("RE"):WaitForChild("Vote"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(234.1999969482422, 193.89999389648438, 21.299999237060547)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(239.39999389648438, 193.89999389648438, 16.899999618530273)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(234.8000030517578, 193.89999389648438, 26.200000762939453)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(234.6999969482422, 193.89999389648438, 15.899999618530273)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(239.60000610351562, 193.89999389648438, 22)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "ShinySwatHaste2",
+    [2] = Vector3.new(239.3000030517578, 193.89999389648438, 26.200000762939453)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "SageBloodLust2",
+    [2] = Vector3.new(218.60000610351562, 193.89999389648438, 14.800000190734863)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "SageBloodLust2",
+    [2] = Vector3.new(219.5, 193.89999389648438, 19.399999618530273)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "SageBloodLust2",
+    [2] = Vector3.new(217.8000030517578, 193.89999389648438, 25.600000381469727)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait()
+local args = {
+    [1] = "SageBloodLust2",
+    [2] = Vector3.new(223.39999389648438, 193.89999389648438, 15.199999809265137)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("deploy"):InvokeServer(unpack(args))
+wait() 
+game:GetService("ReplicatedStorage"):WaitForChild("SharedModules"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("GameOverOptionsService"):WaitForChild("RF"):WaitForChild("voteAgain"):InvokeServer()
+wait()
+
+for i,v in pairs(game:GetService("Workspace").Towers:GetChildren()) do
+if #game:GetService("Workspace").Towers:GetChildren() == 12 then
+local args = {
+    [1] = v
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("rudolph101_tower-defence-towers@0.2.2"):WaitForChild("tower-defence-towers"):WaitForChild("Shared"):WaitForChild("TowerPlacement"):WaitForChild("__comm__"):WaitForChild("RF"):WaitForChild("upgrade"):InvokeServer(unpack(args))
+end
+end
+
+end) 
+end
